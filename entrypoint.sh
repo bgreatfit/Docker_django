594bc4ca1f169e85552d5abea6402231373e2238
micheal <mojemoron2009@yahoo.com>
2019-04-05 08:23:57 +0100

    added nginx,WSGI image

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,12 @@
 # Collect static files
 #echo "Collect static files"
 #python manage.py collectstatic --noinput
-
+python3 manage.py collectstatic --no-input
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+gunicorn --timeout=30 --workers=2 --bind 0.0.0.0:8000 first_project.wsgi:application
+#python manage.py runserver 0.0.0.0:8000
