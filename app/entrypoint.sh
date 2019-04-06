6e450553ed11dd44a3501598a756f20e12c2f68c
micheal <mojemoron2009@yahoo.com>
2019-04-06 09:32:55 +0100

    removed .idea ,added gitignore

---
+++ app/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Collect static files
+#echo "Collect static files"
+#python manage.py collectstatic --noinput
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py flush --no-input
+python manage.py collectstatic --no-input
+exec "$@"
+#gunicorn --timeout=30 --workers=2 --bind 0.0.0.0:8000 first_project.wsgi:application
+#python manage.py runserver 0.0.0.0:8000
