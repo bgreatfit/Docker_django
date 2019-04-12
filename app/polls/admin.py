dae6a22171820928ce01a3ab15231440f3c314ca
micheal <mojemoron2009@yahoo.com>
2019-04-12 21:58:37 +0100

    added1 new travis config

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Question,AccessRecords,WebPage,Topic, Users
+from .models import Question, AccessRecords, WebPage, Topic, Users
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+   # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']}),
+    ]
+
 
 # Register your models here.
-
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(WebPage)
 admin.site.register(AccessRecords)
 admin.site.register(Topic)
