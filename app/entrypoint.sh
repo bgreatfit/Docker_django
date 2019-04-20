fd49137ea7ae9ea063b84efa9605c4ecfa843a58
micheal <mojemoron2009@yahoo.com>
2019-04-20 10:40:53 +0100

    test 49

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,13 +5,13 @@
 #python manage.py collectstatic --noinput
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate
+python ./first_project/app/manage.py migrate
 
 # Start server
 echo "Starting server"
 
-python manage.py flush --no-input
-python manage.py collectstatic --no-input
+python ./first_project/app/manage.py flush --no-input
+python ./first_project/app/manage.py collectstatic --no-input
 
 exec "$@"
 #gunicorn --timeout=30 --workers=2 --bind 0.0.0.0:8000 first_project.wsgi:application
